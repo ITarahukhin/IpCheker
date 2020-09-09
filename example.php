d0cfe6d496c6f10990a5da08096a1385046272c1
ivan-taranukhin <70968921+ivan-taranukhin@users.noreply.github.com>
2020-09-09 13:26:13 +0300

    add example.php

---
+++ example.php
@@ -0,0 +1,41 @@
+<?php
+require_once "vendor/autoload.php";
+
+use App\Dl\Utils\IpChecker;
+
+$checker = new IpChecker();
+
+$checker->setMask(24);
+$checker->setNetwork("192.168.1.0");
+
+$arrIp = [
+    "192.168.0.1",
+    "192.168.0.2",
+    "192.168.0.3",
+    "10.168.0.4",
+];
+
+foreach ($arrIp as $ip) {
+    $arrState[] = [
+        "ip" => $ip,
+        "state" => $checker->setIp($ip)->check()
+    ];
+}
+
+foreach ($arrState as $check) {
+    if ($check['state']) {
+        echo "Данный IP принадлежит этой сети.<br>";
+    } else {
+        echo "Данный IP НЕ принадлежит этой сети.<br>";
+    }
+}
+
+
+$x = (new IpChecker("192.168.15.22", "192.168.15.0", 8))->check();
+
+if ((new IpChecker("192.168.15.22", "192.168.15.0", 8))->check()) {
+    echo "Данный IP принадлежит этой сети.";
+    // some action
+} else {
+    echo "Данный IP НЕ принадлежит этой сети.";
+}
